eac815daf2f8faf6a542382655fa9831421665a1
w.alexander <will_alexander@me.com>
2025-05-29 14:29:34 +0200

    Remove boilerplate, add Tailwind

--- apps/nebulous-narwhal/src/app/app.component.ts
+++ apps/nebulous-narwhal/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'nebulous-narwhal';
 }
