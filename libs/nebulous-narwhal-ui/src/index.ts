cdc758b49718fcceab2b765ff12047e489cab1ed
w.alexander <will_alexander@me.com>
2025-05-29 14:51:01 +0200

    Add stuff

--- libs/nebulous-narwhal-ui/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/nebulous-narwhal-ui.component';
--- libs/nebulous-narwhal-ui/src/lib/nebulous-narwhal-ui.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'nebnar-nebulous-narwhal-ui',
-  templateUrl: './nebulous-narwhal-ui.component.html',
-  styleUrl: './nebulous-narwhal-ui.component.scss',
-})
-export class NebulousNarwhalUiComponent {}
